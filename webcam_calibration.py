b0c8c268cd94a28db0d5eca7349a32cddcb1eb35
Juan Ferrua <juangelo@mit.edu>
2017-06-26 09:17:05 -0400

    Fixed bug of not entering final block of code

--- webcam_calibration.py
+++ webcam_calibration.py
@@ -18,7 +18,7 @@ cap = cv2.VideoCapture(0)
 
 # capture and show mage fram until user presses 'q'
 while cap.isOpened():
-	ret, image = cap.read()
+	image = cap.read()[1]
 	cv2.imshow("Webcam Image", image)
 	if (cv2.waitKey(1) & 0xFF) == ord('q'):		# if user presses 'q'
 		cv2.imwrite("webcamim.png", image)		# save image for debugging/documentation
@@ -29,7 +29,7 @@ cap.release()
 
 # convert image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-print gray.shape
+# print gray.shape
 
 i = 1
 while i > 0:
@@ -40,11 +40,11 @@ while i > 0:
 		i = 0
 
 # find chessboard corners. note: watch out for board row/col dimensions!
-ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
-print "Ret after findChessboardCorners"
-print ret
+retCorners, corners = cv2.findChessboardCorners(gray, (9,6), None)
+# print "Ret after findChessboardCorners"
+# print retCorners
 
-if ret == True:
+if retCorners == True:
 	objpoints.append(objp)
 
 	# not sure where (11,11) comes from
@@ -52,17 +52,16 @@ if ret == True:
 	imgpoints.append(corners)
 	
 	#draw corners
-	cv2.drawChessboardCorners(image, (10,7), corners, ret)
+	cv2.drawChessboardCorners(image, (10,7), corners, True)
 
 #UNDISTORTION + CAMERA MATRICES
 
-print(objpoints.__len__())
-print(imgpoints.__len__())
+# print(objpoints.__len__())
+# print(imgpoints.__len__())
 
 #camera matrices
-print(gray.shape[::-1])
-a = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-ret, mtx, dist, rvecs, tvecs = a
+# Note: retCalibrate is (at least sometimes) a double and not a boolean (what we expected)
+retCalibrate, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 h, w = image.shape[:2]
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
@@ -83,7 +82,8 @@ def draw(img, corners, imgpoints):
 
 axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
 
-if ret == True:
+# for some reason, this is a double instead of a boolean
+if retCalibrate > 0:
 	cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
 	rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners, mtx, dist)[1:]
 
