20515938c3c0cef142b6fd26c940937fcfa7095b
Juan Ferrua <juangelo@mit.edu>
2017-07-06 15:59:33 -0400

    New infrastructure

---
+++ fume_hood_monitor.py
@@ -0,0 +1,94 @@
+from scipy.spatial import distance as dist
+import time
+import datetime
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+import yaml
+import os
+
+
+# threshold to determine if fume hood is "open enough" to trigger alarm
+# sashHeight is a number between 0 and 1 indicating ratio of sash height / maximum height
+HEIGHT_THRESHOLD = 0.5
+
+# threshold to determine if there is enough motion for the sash to be in use
+# motion is a number between 0 and 1 (with a higher value indicating more motion)
+MOTION_THRESHOLD = 0.2
+
+# amount of time the fume hood has to be open and without use before triggering the alarm
+TIME_TO_ALARM = 30	# seconds
+
+# minimum amount of time between two measurements
+SLEEP_INTERVAL = 20
+
+# initialize variable to keep track of time ellapsed between two measurements
+# used for integrating power
+prevTime = datetime.datetime.now()
+
+# initialize variable to keep track of the last time the fume hood was in use
+timeLastUsed = datetime.datetime.now()
+
+# initialize video capture
+cap = cv2.VideoCapture(0)
+
+finished = 0
+while not finished:
+	# time elapsed (in seconds) since last measurement
+	timeElapsed = (datetime.datetime.now() - prevTime).total_seconds()
+
+	# update prevTime with the current time
+	# not sure if timeElapsed and prevTime are in optimal code location
+	# Crucial to make sure all time is accounted for (i.e. prevTime should immediately follow timeElapsed)
+	prevTime = datetime.datetime.now()
+
+	# Checking the degree to which the fume hood is open
+	# NOTE: assume that this takes a non-trivial amount of time
+	try:
+		# TO DO: actually get sash state
+		sashHeight = 0
+	except:
+		print "Error computing sashHeight, defaults to 0"
+		sashHeight = 0
+
+	# Checking for motion
+	# NOTE: assume that this takes a non-trivial amount of time
+	try:
+		# # TO DO: actually check for motion
+		motion = 0
+	except:
+		print "Error computing motion, defaults to 0"
+		motion = 0
+
+	# if there is motion (i.e. the fume hood is currently in use), update the value of timeLastUsed
+	if motion > 0:
+		timeLastUsed = datetime.datetime.now()
+
+	# compute the time elapsed (in seconds) since the fume hood was last used
+	timeSinceUse = (datetime.datetime.now() - timeLastUsed).total_seconds()
+
+	# signal to turn alarm on(1)/off(0)
+	# if the sash is open and hasn't been in use, turn on alarm (under appropriate circumnstances?)
+	# unsure if this is the best logic for turning alarm off
+	alarm = 1 if (sashHeight > HEIGHT_THRESHOLD) and (motion < MOTION_THRESHOLD) and (timeSinceUse > TIME_TO_ALARM) else 0
+	if alarm:
+		print "ALARM ON"
+		# TO DO: actually turn on alarm
+	else:
+		print "ALARM OFF"
+		# TO DO: make sure alarm is off
+
+	# TO DO: add info to data file
+	# include sashState, motion, timeElapsed
+
+	# wait until SLEEP_INTERVAL is over before making the next measurement
+	# listen for 'q' as a signal to quit
+	while (datetime.datetime.now() - prevTime).total_seconds() < SLEEP_INTERVAL:
+	    if cv2.waitKey(1) & 0xFF == ord('q'):
+	        finished = 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
--- motion_detect.py
+++ motion_detect.py
@@ -4,8 +4,6 @@ import imutils
 import time
 import cv2
 
-#binary output
-motion_list = []
 ap = argparse.ArgumentParser()
 #define minimum pixel size of region of frame to be considered actual motion
 ap.add_argument("-a", "--min-area", type=int, default=100, help="minimum area size")
@@ -13,46 +11,49 @@ args = vars(ap.parse_args())
 
 cap = cv2.VideoCapture(0)
 time.sleep(0.25)
-#initialize first frame
-prevFrame = None
-i = 0
-while cap.isOpened() and i < 20:
-    ret, frame = cap.read()
-    start = time.time()
-    #if no frame, end of video
-    if not ret:
-        break
 
-    #resize frame, convert to grayscale, blur
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #21 x 21 region for gaussian smoothing average pixel intensity
-    #smooths out high frequency noise
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+def detect_motion(cap, duration, frameThreshold=25):
+    startTime = datetime.datetime.now()
+    #initialize first frame
+    prevFrame = None
+    i = 0
+    #binary output
+    motion_list = []
+    while cap.isOpened() and (datetime.datetime.now() - startTime).total_seconds() < duration:
+        ret, frame = cap.read()
+        #if no frame, end of video
+        if not ret:
+            print "Capture may be faulty"
 
-    #initialize firstFrame
-    if prevFrame is None:
+        #resize frame, convert to grayscale, blur
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #21 x 21 region for gaussian smoothing average pixel intensity
+        #smooths out high frequency noise
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        #initialize firstFrame
+        if prevFrame is None:
+            prevFrame = gray
+
+        #subtract frames
+        frameDelta = cv2.absdiff(prevFrame, gray)
+        #threshold delta to reveal regions with significan changes in pixel intensity
+        #if delta < 25, disregard, else, count as motion
+        thresh = cv2.threshold(frameDelta, frameThreshold, 255, cv2.THRESH_BINARY)[1]
+        #dilate image(fill in holes with white) and find contours
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        ( _,cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        motion = 0
+        for c in cnts:
+            #if contour is small (too insignificant) then ignore it
+            if cv2.contourArea(c) > args["min_area"]:
+                motion = 1
+                #print motion
+        motion_list.append(motion)
         prevFrame = gray
 
-    #subtract frames
-    frameDelta = cv2.absdiff(prevFrame, gray)
-    #threshold delta to reveal regions with significan changes in pixel intensity
-    #if delta < 25, disregard, else, count as motion
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    #dilate image(fill in holes with white) and find contours
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    ( _,cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    motion = 0
-    for c in cnts:
-        #if contour is small (too insignificant) then ignore it
-        if cv2.contourArea(c) > args["min_area"]:
-            motion = 1
-            #print motion
-    motion_list.append(motion)
-    prevFrame = gray
-
-    i += 1
-
-print motion_list
+    return sum(motion_list)/len(motion_list) if motion_list else 0
+    
 cap.release()
 cv2.destroyAllWindows()
